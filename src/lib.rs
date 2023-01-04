fc0f0af6b99e22d531eb40a3e0942a1cc6fe4648
François Diakhaté <fdiakh@gmail.com>
2023-01-04 23:36:41 +0100

    Add lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+mod collections;
+mod idrange;
+
+pub use idrange::IdRangeTree;
+pub use idrange::IdRangeList;
+pub use collections::IdSet;
+pub use collections::NodeSet;
+pub use collections::IdSetIter;
+pub use collections::NodeSetParseError;
