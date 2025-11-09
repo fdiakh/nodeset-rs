493b0bf8d61e03f787b4917fbecd7458fbab8bf6
François Diakhaté <fdiakh@gmail.com>
2025-11-09 14:16:49 +0100

    style: format C code with clang

--- nodeset-capi/examples/ns_intersect_iter.c
+++ nodeset-capi/examples/ns_intersect_iter.c
@@ -5,68 +5,62 @@
  * This examples shows how to iterate over the intersection of two nodesets
  * using the C API
  */
-int main(int argc, char **argv)
-{
-	char *error;
-	char *node;
-	NodeSetIter *iter;
-	NodeSet *nodeset1, *nodeset2, *intersection;
+int main(int argc, char **argv) {
+    char *error;
+    char *node;
+    NodeSetIter *iter;
+    NodeSet *nodeset1, *nodeset2, *intersection;
 
-	if (argc != 3)
-	{
-		fprintf(stderr, "usage: ns_intersect_iter '<nodeset1>' '<nodeset2>'\n\n");
-		fprintf(stderr, "Example:\n");
-		fprintf(stderr, "\tns_intersect_iter 'n[1-8]' 'n[6-10]'\n");
-		return 1;
-	}
+    if (argc != 3) {
+        fprintf(stderr,
+                "usage: ns_intersect_iter '<nodeset1>' '<nodeset2>'\n\n");
+        fprintf(stderr, "Example:\n");
+        fprintf(stderr, "\tns_intersect_iter 'n[1-8]' 'n[6-10]'\n");
+        return 1;
+    }
 
-	/* Initialize the group resolver with default settings */
-	if (init_default_resolver(&error) != 0)
-	{
-		fprintf(stderr, "error: %s\n", error);
-		ns_free_error(error);
-		return 1;
-	}
+    /* Initialize the group resolver with default settings */
+    if (init_default_resolver(&error) != 0) {
+        fprintf(stderr, "error: %s\n", error);
+        ns_free_error(error);
+        return 1;
+    }
 
-	/* Parse the nodesets */
-	if ((nodeset1 = ns_parse(argv[1], &error)) == NULL)
-	{
-		fprintf(stderr, "error: %s\n", error);
-		ns_free_error(error);
-		return 1;
-	}
+    /* Parse the nodesets */
+    if ((nodeset1 = ns_parse(argv[1], &error)) == NULL) {
+        fprintf(stderr, "error: %s\n", error);
+        ns_free_error(error);
+        return 1;
+    }
 
-	if ((nodeset2 = ns_parse(argv[2], &error)) == NULL)
-	{
-		fprintf(stderr, "error: %s\n", error);
-		ns_free_error(error);
-		return 1;
-	}
+    if ((nodeset2 = ns_parse(argv[2], &error)) == NULL) {
+        fprintf(stderr, "error: %s\n", error);
+        ns_free_error(error);
+        return 1;
+    }
 
-	intersection = ns_intersection(nodeset1, nodeset2);
-	/* Create an iterator over the nodes in the nodeset */
-	iter = ns_iter(intersection);
+    intersection = ns_intersection(nodeset1, nodeset2);
+    /* Create an iterator over the nodes in the nodeset */
+    iter = ns_iter(intersection);
 
-	/* Iterate over the nodes */
-	while ((node = ns_iter_next(iter, &error)) != NULL)
-	{
-		printf("%s\n", node);
-		ns_free_node(node);
-	}
+    /* Iterate over the nodes */
+    while ((node = ns_iter_next(iter, &error)) != NULL) {
+        printf("%s\n", node);
+        ns_free_node(node);
+    }
 
-	/* Check whether the iterator ended due to an error */
-	if (ns_iter_status(iter) != 0)
-	{
-		fprintf(stderr, "error: %s\n", error);
-		ns_free_error(error);
-		return 1;
-	}
+    /* Check whether the iterator ended due to an error */
+    if (ns_iter_status(iter) != 0) {
+        fprintf(stderr, "error: %s\n", error);
+        ns_free_error(error);
+        return 1;
+    }
 
-	/* Free the iterator and nodeset */
-	ns_free_iter(iter);
-	ns_free_nodeset(intersection);
-	ns_free_nodeset(nodeset1);
-	ns_free_nodeset(nodeset2);
+    /* Free the iterator and nodeset */
+    ns_free_iter(iter);
+    ns_free_nodeset(intersection);
+    ns_free_nodeset(nodeset1);
+    ns_free_nodeset(nodeset2);
 
-	return 0;
+    return 0;
 }
--- nodeset-capi/examples/ns_list.c
+++ nodeset-capi/examples/ns_list.c
@@ -4,45 +4,40 @@
 /*
  * This examples shows how to list nodes in a nodeset using the C API
  */
-int main(int argc, char **argv)
-{
-	char *error;
-	char **nodes;
-	size_t len;
-	int i;
+int main(int argc, char **argv) {
+    char *error;
+    char **nodes;
+    size_t len;
+    int i;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: ns_list '<nodeset>'\n\n");
-		fprintf(stderr, "Example:\n");
-		fprintf(stderr, "\tns_list 'n[1-10]!n[2-9]'\n");
-		return 1;
-	}
+    if (argc != 2) {
+        fprintf(stderr, "Usage: ns_list '<nodeset>'\n\n");
+        fprintf(stderr, "Example:\n");
+        fprintf(stderr, "\tns_list 'n[1-10]!n[2-9]'\n");
+        return 1;
+    }
 
-	/* Initialize the group resolver with default settings */
-	if (init_default_resolver(&error) != 0)
-	{
-		fprintf(stderr, "error: %s\n", error);
-		ns_free_error(error);
-		return 1;
-	}
+    /* Initialize the group resolver with default settings */
+    if (init_default_resolver(&error) != 0) {
+        fprintf(stderr, "error: %s\n", error);
+        ns_free_error(error);
+        return 1;
+    }
 
-	/* Compute a list of nodes and store it an array */
-	if (ns_list(argv[1], &len, &nodes, &error) != 0)
-	{
-		fprintf(stderr, "error: %s\n", error);
-		ns_free_error(error);
-		return 1;
-	}
+    /* Compute a list of nodes and store it an array */
+    if (ns_list(argv[1], &len, &nodes, &error) != 0) {
+        fprintf(stderr, "error: %s\n", error);
+        ns_free_error(error);
+        return 1;
+    }
 
-	/* Display each node */
-	for (i = 0; i < len; ++i)
-	{
-		printf("%s\n", nodes[i]);
-	}
+    /* Display each node */
+    for (i = 0; i < len; ++i) {
+        printf("%s\n", nodes[i]);
+    }
 
-	/* Free the nodes array */
-	ns_free_node_list(nodes, len);
+    /* Free the nodes array */
+    ns_free_node_list(nodes, len);
 
-	return 0;
+    return 0;
 }
